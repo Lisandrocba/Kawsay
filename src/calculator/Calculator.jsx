2889d260179639810caf57cda617f48885db2fd8
lisandro cordoba <lisandrocba_7@hotmail.com>
2023-10-30 15:21:58 -0300

    creando atomos y moleculas

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Button, Jumbotron } from "react-bootstrap";
+import { Start } from "../atomos";
+
 
 const initialState = {
   //electrico
@@ -155,45 +157,15 @@ export default function Calculator() {
         className="cover"
         style={{ backgroundImage: `url("http://i.ibb.co/jgZph3V/fondo.png")` }}
       >
-        {/* start */}
-        {location === "start" ? (
-          <div className="min-vh-100">
-            <div>
-              <h1 className="my-2 parH text-left font-weight-bold pl-2">
-                <span className="titleH">¡Bienvenido a </span>Kawsay!
-              </h1>
-            </div>
-            <div className="shadow text-left w-50 parHB">
-              <p className="parH mt-5 pl-2 pt-2">
-                <h5>
-                  ¿Sabías que con nuestro estilo de vida moderno, cada persona
-                  genera alrededor de cuatro toneladas de CO2 al año?
-                </h5>
-              </p>
-              <p className="parH pb-3 pl-2">
-                Una de las principales causas del calentamiento global es la
-                emisión de gases de efecto invernadero, entre ellos y
-                principalmente, el CO2. La marca que se deja sobre el medio
-                ambiente con cada actividad que emite gases de efecto
-                invernadero es conocida como huella de carbono.
-              </p>
-            </div>
-            <div>
-              <p className="parH mb-n3 mt-3 py-3 pl-2">
-                <h5 className="font-weight-bold">
-                  ¿Te gustaria conocer tu huella de Carbono?
-                </h5>
-              </p>
-              <div className="ml-5 mt-2">
-                <Button variant="flat" size="lg" onClick={() => handleButton()}>
-                  CALCULAR
-                </Button>
-              </div>
-            </div>
-          </div>
-        ) : (
+        {
+        location === "start" ? 
+          
+          <Start handleButton = {handleButton} />
+        : (
           <div />
-        )}
+        )
+        }
+
         {/* question1 */}
         {location === "question1" ? (
           <div>
